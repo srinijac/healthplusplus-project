c7378e59a8f09e0b91486ba05054e9d2cc2a4104
srinijac <srinijac23@gmail.com>
2019-10-20 13:06:17 -0700

    Revert "Merge branch 'master' of https://github.com/srinijac/healthplusplus-project"
    This reverts commit c1f76ac8c490e141b54417b0a87cd3424087a877, reversing
    changes made to 9a1172b217a879397182415d73fc93ec7f671835.

--- unrelated_react_code/button.js
+++ unrelated_react_code/button.js
@@ -1,16 +1,16 @@
 class Button{
   constructor(props){
     this.props = props
-    this.state = {
+    self.state = {
       text: "this button has not been clicked"
     }
   }
   render(){
     return <button classname = "Button" onClick = ()=>{
-      this.state.text = "this button got clicked!"
-    }>testing button here. The test says: {this.state.text} </button>
+      self.state.text = "this button got clicked!"
+    }>testing button here. The test says: {self.state.text} </button>
   }
 }
 
 const cont = document.querySelector('#reactButton');
-reactDOM.render(React.createElement(Button), cont)
+reactDOM.render(Button)
