e4039edff0683b0e43fa101890ea6e1ab410d319
sumguy002 <bryanwu123456@gmail.com>
2019-10-20 12:31:37 -0700

    The button isn't showing. Debug attempted and failed

--- unrelated_react_code/button.js
+++ unrelated_react_code/button.js
@@ -1,16 +1,16 @@
 class Button{
   constructor(props){
     this.props = props
-    self.state = {
+    this.state = {
       text: "this button has not been clicked"
     }
   }
   render(){
     return <button classname = "Button" onClick = ()=>{
-      self.state.text = "this button got clicked!"
-    }>testing button here. The test says: {self.state.text} </button>
+      this.state.text = "this button got clicked!"
+    }>testing button here. The test says: {this.state.text} </button>
   }
 }
 
 const cont = document.querySelector('#reactButton');
-reactDOM.render(Button)
+reactDOM.render(React.createElement(Button), cont)
