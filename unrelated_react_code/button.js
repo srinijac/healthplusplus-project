cda9cf41787ea136601677930d6892182e93fbf6
sumguy002 <bryanwu123456@gmail.com>
2019-10-20 12:01:45 -0700

    Button functionality added. Bugs likely

--- unrelated_react_code/button.js
+++ unrelated_react_code/button.js
@@ -1,3 +1,16 @@
 class Button{
-
+  constructor(props){
+    this.props = props
+    self.state = {
+      text: "this button has not been clicked"
+    }
+  }
+  render(){
+    return <button classname = "Button" onClick = ()=>{
+      self.state.text = "this button got clicked!"
+    }>testing button here. The test says: {self.state.text} </button>
+  }
 }
+
+const cont = document.querySelector('#reactButton');
+reactDOM.render(Button)
