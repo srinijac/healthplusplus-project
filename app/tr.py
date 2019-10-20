9a1172b217a879397182415d73fc93ec7f671835
srinijac <srinijac23@gmail.com>
2019-10-20 12:47:20 -0700

    inputting phone number

--- app/tr.py
+++ app/tr.py
@@ -1,27 +1,23 @@
 from twilio.rest import Client
-# import cgi
-# import cgitb
-# cgitb.enable()
-#
-# form = cgi.FieldStorage()
-# #
-# # print(type(form))
-# # print(form)
-# first = form.getvalue('first_name')
-# # print(first)
-# last = form.getvalue('last_name')
 
-def sendsms(first, last):
+# Your Account SID from twilio.com/console
+account_sid = "ACa3ee54b5030f3a2b427107e4ff8517af"
+# Your Auth Token from twilio.com/console
+auth_token  = "5f5479d1ea747a53454b61d812376cc4"
 
-    # Your Account SID from twilio.com/console
-    account_sid = "ACa3ee54b5030f3a2b427107e4ff8517af"
-    # Your Auth Token from twilio.com/console
-    auth_token  = "5f5479d1ea747a53454b61d812376cc4"
+client = Client(account_sid, auth_token)
 
-    client = Client(account_sid, auth_token)
+def addnumber(num):
+    validation_request = client.validation_requests \
+                           .create(
+                                friendly_name='Contact Number',
+                                phone_number=str(num)
+                            )
+    print(validation_request.friendly_name)
 
+def sendsms(first, last, number):
     message = client.messages.create(
-        to="+14074373965",
+        to=str(number),
         from_="+12052930681",
         body=("your name is " + first + " " + last))
     return message
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ import json
 from app.forms import LoginForm
 from app import app
 from app import tr
-from app.tr import sendsms
+from app.tr import sendsms, addnumber
 
 #twilio
 from twilio.rest import Client
@@ -27,9 +27,13 @@ def login():
     return render_template("login.html")
 
 @app.route('/tr', methods=['GET', 'POST'])
-def smstest():
+def login():
     first = request.values.get("first_name")
     last = request.values.get("last_name")
-    tr.sendsms(first, last)
+    number = request.values.get("to_phone_num")
+    message = "You're signed up! Welcome to ___, %s %s%s%s!"
+    print(number)
+    #tr.addnumber(number)
+    tr.sendsms(message, number first, last)
     # os.system('python tr.py')
     return render_template("index.html")
