a6bac688cd1674090cc358db76a81daed4e3d479
srinijac <srinijac23@gmail.com>
2019-10-20 02:35:07 -0700

    so many freaking changes

--- app/tr.py
+++ app/tr.py
@@ -1,4 +1,11 @@
 from twilio.rest import Client
+import cgi
+import cgitb #found this but isn't used?
+
+inputs = cgi.FieldStorage()
+
+first_name = inputs.getvalue('first_name').capitalize()
+last_name  = inputs.getvalue('last_name').capitalize()
 
 # Your Account SID from twilio.com/console
 account_sid = "ACa3ee54b5030f3a2b427107e4ff8517af"
@@ -10,6 +17,20 @@ client = Client(account_sid, auth_token)
 message = client.messages.create(
     to="+14074373965",
     from_="+12052930681",
-    body="wassup bro how's ur shit")
+    body="your name is", first_name, last_name)
 
 print(message.sid)
+
+#
+#
+#
+#
+# print ("Content-type:text/html\r\n\r\n")
+# print ("<html>")
+# print ("<head>")
+# print ("<title>Hello - Second CGI Program</title>")
+# print ("</head>")
+# print ("<body>")
+# print ("<h2>Your name is {}. {} {}</h2>".format(last_name, first_name, last_name))
+# print ("</body>")
+# print ("</html>")
--- app/views.py
+++ app/views.py
@@ -12,11 +12,15 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/login', methods=['GET', 'POST'])
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     # if form.validate_on_submit():
+#     #     flash('Login requested for user {}, remember_me={}'.format(
+#     #         form.username.data, form.remember_me.data))
+#     #     return redirect(url_for('index.html'))
+#     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/login')
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index.html'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template("thissucks.html")
