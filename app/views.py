e9697b0642ed07520cae82ed0a02ea5cd59a70a1
srinijac <srinijac23@gmail.com>
2019-10-19 19:34:58 -0700

    test page!

--- app/views.py
+++ app/views.py
@@ -6,4 +6,8 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("test.html")
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
